96496296d87ab17e2c16d5fb07a9b8f06d159821
KazutoshiHorii <chokokzts@gmail.com>
2025-10-16 10:29:36 +0900

    Delete docs directory

--- docs/config.js
+++
@@ -1,5 +0,0 @@
-window.COMMUNITY_MAP_CONFIG = {
-  GAS_ENDPOINT: 'https://script.google.com/macros/s/AKfycbxMyMkqYFkEBoKTXf_Ajo66ubgAEsT4f7TV3FkB-xqTVS3VeRUIdez5CF2KUf0NyzI3g/exec',
-  CENTER: { lat: 35.2961, lng: 135.1300 }, // 福知山市中心
-  ZOOM: 15
-};
